50066e2e4816f98b017a6168c662a1c443a634ec
Brendan Lawlor <brendan.lawlor@gmail.com>
2013-06-30 20:04:15 +0200

    Added two new (empty) screens that appear when the first two buttons (people and places). Serve as examples for how to make the other buttons work.

--- Sean/src/main/java/ie/lawlor/sean/MainActivity.java
+++ Sean/src/main/java/ie/lawlor/sean/MainActivity.java
@@ -1,16 +1,21 @@
 package ie.lawlor.sean;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpMenuButtons();
     }
+
 
 
     @Override
@@ -19,5 +24,36 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    private void setUpMenuButtons() {
+        Button peopleButton = (Button) findViewById(R.id.peopleMenuButton);
+        peopleButton.setOnClickListener(this);
+        Button placesButton = (Button) findViewById(R.id.placesMenuButton);
+        placesButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.peopleMenuButton:
+                showPeople();
+                break;
+            case R.id.placesMenuButton:
+                showPlaces();
+                break;
+        }
+    }
+
+
+    private void showPeople() {
+        Intent showPeopleActivity = new Intent(this, PeopleActivity.class);
+        startActivity(showPeopleActivity);
+    }
+
+
+    private void showPlaces() {
+        Intent showPlacesActivity = new Intent(this, PlacesActivity.class);
+        startActivity(showPlacesActivity);
+
+    }
 }
